fc1081fe7f2045ce049864d9269868a587aa7819
AMANPATEL <amanpatel8844382@gmail.com>
2024-08-08 17:42:12 +0530

    useReducer Hook(Simple state,complex state),Multiple useReducer,Use Reducer with useContext,Featchingdata with useReducer(1,2),Use State vs useReducer Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component, useReducer } from "react";
+import CounterOne from "./components/CounterOne";
+import CounterTwo from "./components/CounterTwo";
+import CounterThree from "./components/CounterThree";
+import ComponentB from "./components/ComponentB";
+import ComponentC from "./components/ComponentC";
+import ComponentA from "./components/ComponentA";
+import DataFeatchingOne from "./components/DataFeatchingOne";
+import DataFeatchingTwo from "./components/DataFeatchingTwo";
+export const CountContext = React.createContext();
+
+// const initialState = 0;
+
+// const reducer = (state, action) => {
+//   switch (action) {
+//     case "increment":
+//       return state + 1;
+//     case "decrement":
+//       return state - 1;
+//     case "reset":
+//       return initialState;
+//     default:
+//       return state;
+//   }
+// };
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [count, dispatch] = useReducer(reducer, initialState);
+  // return (
+  //   <CountContext.Provider
+  //     value={{ countState: count, countDispatch: dispatch }}
+  //   >
+  <div className="App">
+    <DataFeatchingTwo />
+    {/* <DataFeatchingOne /> */}
+    {/* Counter - {count}
+        <ComponentA />
+        <ComponentB />
+        <ComponentC /> */}
+    {/* <CounterThree /> */}
+    {/* <CounterTwo /> */}
+    {/* <CounterOne/> */}
+  </div>;
+  // </CountContext.Provider>
+  // );
 }
 
 export default App;
